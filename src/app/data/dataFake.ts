0c816f35a18afdcd29de81d081fbd92e423425f0
DevMau <mauriciolxrdev@gmail.com>
2025-03-15 21:35:31 -0300

    Subindo versão final do projeto para o github

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Principais métricas de avaliação de modelos em Machine Learning",
+    "description": "As principais métricas de avaliação de modelos em Machine Learning variam conforme o tipo de problema, seja ele de classificação, regressão ou agrupamento. Para classificação, métricas como acurácia, precisão, recall e F1-score são essenciais para medir o desempenho do modelo em prever corretamente as classes. Em problemas de regressão, utilizam-se métricas como erro quadrático médio (MSE), erro absoluto médio (MAE) e R², que avaliam a diferença entre os valores preditos e reais. Já para agrupamento, métricas como coeficiente de silhueta e índice de Davies-Bouldin ajudam a medir a coesão e separação dos clusters formados. A escolha da métrica adequada depende dos objetivos do modelo e do impacto dos erros nas decisões de negócio.",
+    "photoCover":"https://cdn-images-1.medium.com/fit/c/430/285/0*zAfBm4bDUSRv0cpf"
+  },
+  {
+    "id":"2",
+    "title": "Afinal, o que é LLMOps? 🤖",
+    "description": "O que é LLMOps? LLMOps é uma extensão do MLOps, focada especificamente na operacionalização de modelos de linguagem de grande porte (LLMs). Ele abrange desde o treinamento e fine-tuning até a implantação e monitoramento contínuo dos modelos em produção. Para garantir eficiência e escalabilidade, LLMOps envolve práticas como versionamento de modelos, otimização de inferência, uso de pipelines automatizados e integração com APIs e bancos de dados. Além disso, inclui estratégias para controle de custos, mitigação de viés e segurança, assegurando que os modelos forneçam respostas coerentes, seguras e alinhadas aos objetivos da aplicação." ,
+    "photoCover":"https://miro.medium.com/v2/resize:fit:1100/format:webp/1*okKDZOj1nobsvx1ZJPMpKw.png"
+  }
+]
